f7a89d8f05f7ef4905e0ca7088d6251fd17fece0
hananenyc <hanane.bibintich@gmail.com>
2023-05-13 00:50:33 -0400

    my first commit

--- src/class18/hw2.java
+++ src/class18/hw2.java
@@ -1,4 +1,21 @@
 package class18;
 
 public class hw2 {
+    /*Create 1 class with a static method that has 3 overloaded forms.
+   Then call each overloaded method with specific arguments and observe result.*/
+    static  void divide(int a,int b){
+        System.out.println(a/b);
+    }
+    static void divide(double a,double b){
+        System.out.println(a/b);
+    }
+    static void divide(double a,int b){
+        System.out.println(a/b);
+    }
+
+    public static void main(String[] args) {
+        divide(10,2);
+        divide(15.5,2.6);
+        divide(22.5,9);
 }
+    }
